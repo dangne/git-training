a44b62cdfba95be74a0fe4f430d5729cc58c60d2
lnakhuong <lnakhuong@gmail.com>
2019-02-19 17:52:10 +0700

    fuckuDang

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void multiply(float a, float b){
+    cout << "Input 2 number" << endl;
+    cin >> a >> b;
+    cout << a * b;
+}
+
 int main(){
-    // Hello this is Dang    
+    // Hello this is Dang
     return 0;
 }
