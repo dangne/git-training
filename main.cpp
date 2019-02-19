78b1ee1e83c55239a626c2937a08471cb50f2f15
NhanNgocThien <nhanthien1012@gmail.com>
2019-02-19 17:50:49 +0700

    Day la ham cos

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void Cos_Functions( float X ){
+    if ( cos(X)<(-1) || cos(X)>1 ) cout<<"Input khong hop le\n";
+        else cout<<cos(X);
+}
+
 int main(){
-    // Hello this is Dang    
+    // Hello this is Dang
     return 0;
 }
