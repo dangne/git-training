cce551ea1248ef4debb930c84f15f9f99e003eed
d.huy237 <d.huy723@gmail.com>
2019-02-19 17:49:08 +0700

    add mu_n branch new_Huy

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void Mu_n(int a, int n)
+{
+  int a, n;
+  cout << a^n << endl;
+}
 int main(){
-    // Hello this is Dang    
+
+    // Hello this is Dang
     return 0;
 }
