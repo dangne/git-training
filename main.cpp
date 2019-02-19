5b632a3be7874eb11642ca313011627025a94008
Nguyen Minh Dang <dang.nguyen.bkit@hcmut.edu.vn>
2019-02-19 17:58:37 +0700

    Add main menu

--- main.cpp
+++ main.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 int main(){
-    // Hello this is Dang    
+    // Menu chinh
+    cout << "=== Menu ===" << endl;
+    cout << "1. Cong" << endl;
+    cout << "2. Tru" << endl;
+    cout << "3. Nhan" << endl;
+    cout << "4. Chia" << endl;
+    cout << "5. Mu n" << endl;
+    cout << "6. Sin" << endl;
+    cout << "7. Cos" << endl;
+    cout << "8. Tan" << endl;
+    cout << "9. Cotan" << endl;
+    cout << endl << "Input your choice: ";
+    cin >> cmd;
+
+    switch (case):
+        1:;
+        2:;
+        3:;
+        4:;
+        5:;
+        6:;
+        7:;
+        8:;
+        9:;
+        default: cout << "Invalid\n";
     return 0;
 }
