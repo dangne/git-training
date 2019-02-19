d0cc44837492f8e292387d8c50370ff04a0d0f0a
Nguyen Minh Dang <36122564+dangne@users.noreply.github.com>
2019-02-19 22:20:54 +0700

    Remove redundant code
    Điều kiện bên trong if là điều kiện luôn luôn sai, không cần thiết.
    Bên trong hàm phải cho user input X nữa chứ không truyền tham số X vô hàm

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void Cos_Functions( float X ){
-    if ( cos(X)<(-1) || cos(X)>1 ) cout<<"Input khong hop le\n";
-        else cout<<cos(X);
+void Cos_Functions(){
+    cout << "Input X = "; cin >> X;
+    else cout << cos(X);
 }
 
 int main(){
