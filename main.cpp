c8c62c9530bd1906a902438997d4f259260014c0
Nguyen Minh Dang <36122564+dangne@users.noreply.github.com>
2019-02-19 22:32:51 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 
 void Cos_Functions(){
+	float X;
     cout << "Input X = "; cin >> X;
     else cout << cos(X);
 }
-void multiply(float a, float b){
+void multiply(){
+	float a, b;
     cout << "Input 2 number" << endl;
     cin >> a >> b;
     cout << a * b;
 }
-int main(){
-    // Hello this is Dang
-    return 0;
-}
-void Subtract()
-{
+void Subtract(){
 	float a, b;
 	cout << "Input 2 numbers:";
 	cin >> a >> b;
 	cout << endl << "Subtract 2 number, ans= " << a - b << endl;
+}
+int main(){
+    // Hello this is Dang
+    return 0;
 }
